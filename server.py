c886e5d860dac0f2b47fe5289ce7f5b996eb827b
Emile <emile@teasquat.com>
2016-11-26 19:05:19 +0100

    inital commit

---
+++ server.py
@@ -0,0 +1,60 @@
+import random
+import socket
+import sys
+from thread import *
+
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 7788 # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+players = dict([])
+
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+s.listen(10)
+print 'Socket now listening'
+
+def clientthread(conn):
+    id=random.randint(0,100000)
+    global players
+
+    while True:
+        #Receiving from client
+        data = conn.recv(1024)
+        if data:
+            action, value = data.split("_")
+            if action == "up":
+                print("up")
+                for k, v in players.iteritems():
+                    conn.sendall(v)
+                #update the dude
+            elif action == "pl":
+                print("pl")
+                x,y,dx,dy = value.split(":")
+                assert(x and y and dx and dy)
+                players[id]=value
+                #update his pos
+            elif action == "pt":
+                print("pt")
+                #update his pet
+        else:
+            break
+
+    del players[id]
+    conn.close()
+
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn, ))
+
+s.close()
